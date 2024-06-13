f59063b9ea2b80a360deac4bec7d61fb83b6d9bd
lkehler <Leon.Kehler@outlook.com>
2024-06-13 21:01:13 +0200

    working paillier encryption

--- code_a2.py
+++ code_a2.py
@@ -3,70 +3,115 @@ import matplotlib.pyplot as plt
 import cvxpy as cp
 import time
 from phe import paillier
+from helpers import admm_analysis
+import copy
 
 
 class Mediator:
     def __init__(self, x_bar0):
         self.x_bar = [x_bar0]
+        self.xi_buffer = {}
+
+        # crypto
+        self.pub_k, self.pri_k = paillier.generate_paillier_keypair()
+        # for reset
+        self._initial_state = copy.deepcopy(self.__dict__)
+
+    def reset(self, agents):
+        self.__dict__ = copy.deepcopy(self._initial_state)
+        for agent in agents:
+            agent.reset()
+
+    def receive_xi(self, agent_id, xi):
+        self.xi_buffer[agent_id] = xi
+
+    def calc_xbar(self, encrypted=False):
+        n = len(self.xi_buffer)
+        x_bar_update = np.sum([self.xi_buffer[agent] for agent in self.xi_buffer.keys()]) / n
+        if encrypted:
+            x_bar_update = self.pub_k.encrypt(x_bar_update)
+        self.x_bar.append(x_bar_update)
+
+    def run_admm(self, agents, encrypted=False, plotting=False):
+        iter_max = 18
+        times = []
+        start_time = time.perf_counter()
+        for k in range(iter_max):
+            iter_start_time = time.perf_counter()
+
+            for agent in agents:
+                xi = agent.update_x(self)
+                self.receive_xi(agent.id, xi)
+
+            self.calc_xbar(encrypted=encrypted)
+
+            for agent in agents:
+                agent.update_u(self)
+
+            iter_end_time = time.perf_counter()
+            times.append(iter_end_time - iter_start_time)
+        end_time = time.perf_counter()
+        print(f"The whole ADMM took {np.round(end_time - start_time, 2)}s.")
+        if plotting:
+            admm_analysis(iter_max, agents, self, times)
 
 
 class Agent:
     agents = []
 
-    def __init__(self, x0, u0=0., q=1):
+    def __init__(self, id_, x0, u0=0., q=1):
+        self.id = id_
         self.x = [x0]
         self.u = [u0]
         self.q = q
         Agent.agents.append(self)
 
+        # for reset
+        self._initial_state = copy.deepcopy(self.__dict__)
+
+    def reset(self):
+        self.__dict__ = copy.deepcopy(self._initial_state)
+
+    def update_x(self, mediator):
+        rho = 1
+        # x_ = cp.Variable(1)
+        # # debug start
+        # print("debug:")
+        # print(1 ** 2 * self.q + rho / 2 * cp.norm(1 - mediator.x_bar[-1] + self.u[-1], 2) ** 2)
+        # # debug end
+        # cost = x_ ** 2 * self.q + rho / 2 * cp.norm(x_ - mediator.x_bar[-1] + self.u[-1], 2) ** 2
+        # problem = cp.Problem(cp.Minimize(cost))
+        # problem.solve()
+        # self.x.append(x_.value[0])
+        # return x_.value[0]
+        x_ = rho * (mediator.x_bar[-1] - self.u[-1]) / (2 * self.q + rho)
+        if isinstance(x_, paillier.EncryptedNumber):
+            x_ = mediator.pri_k.decrypt(x_)
+        self.x.append(x_)
+        return x_
+
+    def update_u(self, mediator):
+        u_ = self.u[-1] + self.x[-1] - mediator.x_bar[-1]
+        self.u.append(u_)
+        return u_
+
 
 iter_max = 18
 rho = 1
 
-agent1 = Agent(1)
-agent2 = Agent(0.3)
-agent3 = Agent(0.1)
+agent1 = Agent(1, 1)
+agent2 = Agent(2, 0.3)
+agent3 = Agent(3, 0.1)
 mediator = Mediator(0.5)
 
-times = []
-start_time = time.perf_counter()
-for k in range(iter_max):
-    iter_start_time = time.perf_counter()
-    for agent in Agent.agents:
-        x_update = cp.Variable(1)
-        cost = x_update**2 * agent.q + rho / 2 * cp.norm(x_update - mediator.x_bar[-1] + agent.u[-1], 2)**2
-        problem = cp.Problem(cp.Minimize(cost))
-        problem.solve()
-        agent.x.append(x_update.value[0])
+print("\nQuestion 1")
+mediator.run_admm(Agent.agents, plotting=True)
 
-    # xbar update
-    n = len(Agent.agents)
-    x_bar_update = 1 / n * np.sum([agent.x[-1] for agent in Agent.agents])
-    mediator.x_bar.append(x_bar_update)
+print("\nQuestion 2")
+mediator.reset(Agent.agents)
+mediator.run_admm(Agent.agents, encrypted=True, plotting=True)
 
-    for agent in Agent.agents:
-        u_update = agent.u[-1] + agent.x[-1] - mediator.x_bar[-1]
-        agent.u.append(u_update)
-    iter_end_time = time.perf_counter()
-    times.append(iter_end_time - iter_start_time)
-end_time = time.perf_counter()
-print(f"The whole iteration took {np.round(end_time - start_time, 2)}s.")
+print("\nQuestion 3")
+mediator.reset(Agent.agents)
 
-x_ticks = np.arange(0, iter_max + 1)
-
-print("Question 1")
-plt.figure(1)
-plt.plot(x_ticks, agent1.x, label=r"$x_1$")
-plt.plot(x_ticks, agent2.x, label=r"$x_2$")
-plt.plot(x_ticks, agent3.x, label=r"$x_3$")
-plt.plot(x_ticks, mediator.x_bar, label=r"$\bar{x}$")
-plt.xlabel("iteration number")
-plt.legend()
-plt.show()
-
-plt.figure(2)
-plt.plot(np.arange(0, iter_max), times)
-plt.xlabel("iteration number")
-plt.ylabel("time [s]")
-plt.show()
 
